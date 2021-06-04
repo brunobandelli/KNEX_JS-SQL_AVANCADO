a382df12aecf763040880f60aedc464c02c3494c
brunobandelli <brunobandelli@gmail.com>
2021-06-04 16:08:44 -0300

    QUERY AND NESTED QUERY

--- index.js
+++ index.js
@@ -22,3 +22,22 @@ database.insert(dados).into("games").then(data => {
     console.log(err);
 });
 */
+
+/*SELECT
+database.select(["id","preco"]).table("games").then(data => {
+    console.log(data)
+}).catch(err => {
+    console.log(err)
+})
+*/
+
+
+database.insert({nome: "Beyblade", preco: 30}).into("games").then(data => {
+    database.select(["id","preco"]).table("games").then(data => {
+        console.log(data)
+    }).catch(err => {
+        console.log(err)
+    })
+}).catch(err => {
+    console.log(err);
+});
