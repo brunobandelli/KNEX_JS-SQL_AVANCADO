210de17a48259cbea25211e2afea4cc2b92bf4aa
brunobandelli <brunobandelli@gmail.com>
2021-06-04 23:29:31 -0300

    WHERE, RAW, DELETE, UPDATE

--- index.js
+++ index.js
@@ -42,3 +42,39 @@ database.insert({nome: "Beyblade", preco: 30}).into("games").then(data => {
     console.log(err);
 });
 */
+
+/*WHERE
+var query = database.select()
+    .whereRaw("nome = 'Beyblade' OR preco > 20")
+    .table("games").then(data => {
+        console.log(data)
+    }).catch(err => {
+        console.log(err);
+    });
+*/
+
+
+/*RAW
+database.raw("SELECT * FROM games").then(data => {
+    console.log(data)
+}).catch(err => {
+    console.log(err)
+})
+*/
+
+/*DELETE
+database.where({id: 34}).delete().table("games").then(data => {
+    console.log(data);
+}).catch(err => {
+    console.log(err);
+})
+*/
+
+/*UPDATE
+database.where({id: 32}).update({preco: 10}).table("games").then(data => {
+    console.log(data)
+}).catch(err => {
+    console.log(err)
+})
+*/
+
