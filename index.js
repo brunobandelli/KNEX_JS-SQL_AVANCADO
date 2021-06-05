771e29d4b469af6a48a35a962da4a56734b24c0e
brunobandelli <brunobandelli@gmail.com>
2021-06-05 17:55:57 -0300

    INNERJOIN 1 PARA 1/ WHERE COM JOIN/ 1 PARA M
    INNERJOIN 1 PARA 1/
    WHERE COM JOIN/
    1 PARA M

--- index.js
+++ index.js
@@ -97,3 +97,41 @@ database.insert({
 })
 */
 
+/*INNERJOIN 1 PARA 1
+database.select(["games.*", "estudios.nome as estudio_nome"]).table("games").innerJoin("estudios", "estudios.game_id", "games.id").then(data => {
+    console.log(data);
+}).catch(err => {
+    console.log(err);
+})
+*/
+
+/* WHERE COM JOIN
+database.select(["games.*", "estudios.nome as estudio_nome"]).table("games").innerJoin("estudios", "estudios.game_id", "games.id").where("games.id",14).then(data => {
+    console.log(data);
+}).catch(err => {
+    console.log(err);
+})
+*/
+
+
+/* 1 PARA M
+database.select(["games.*", "estudios.nome as estudio_nome"]).table("games").innerJoin("estudios", "estudios.game_id", "games.id").then(data => {
+    var estudioGamesArray = data;
+    var game = {
+        id: 0,
+        nome: "",
+        estudios: []
+    }
+
+    game.id = data[0].id;
+    game.nome = data[0].nome;
+
+    data.forEach(estudio => {
+        game.estudios.push({nome: estudio.estudio_nome});
+    })
+    console.log(game)
+}).catch(err => {
+    console.log(err);
+})
+*/
+
